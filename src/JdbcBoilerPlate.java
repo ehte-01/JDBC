ef2da555f37a388b0a240edd297de18b0fa5717a
MOHAMMAD EHTESHAM <infoehtesham7886@gmail.com>
2026-02-11 23:18:20 +0530

    first commit

---
+++ src/JdbcBoilerPlate.java
@@ -0,0 +1,28 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Main {
+
+    private static String url = "jdbc:mysql://127.0.0.1:3306/?user=root";
+    private static String username = "root";
+    private static String password = "root";
+
+    public static void main(String[] args) {
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            Connection connection = DriverManager.getConnection(url, username, password);
+            Statement statement = connection.createStatement();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+}
