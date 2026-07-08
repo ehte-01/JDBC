3904c778c11fa1067e6beda0730960dbdbbd229f
ehte-01 <infoehtesham7886@gmail.com>
2026-07-08 16:41:31 +0530

    Revision jdbc

---
+++ src/ConnectingJavatoSql.java
@@ -0,0 +1,42 @@
+import java.sql.*;
+public class ConnectingJavatoSql {
+    public static void main(String[] args) throws ClassNotFoundException {
+        String url= "jdbc:mysql://localhost:3306/mydatabase";
+        String username="root";
+        String password="root";
+        String query="Select * from employees;";
+
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("Drivers loaded successfully!!");
+        }catch(ClassNotFoundException e){
+            System.out.println(e.getMessage());
+        }
+
+        try{
+            Connection con= DriverManager.getConnection(url,username,password);
+            System.out.println("Connection established successfully");
+            Statement stmt=con.createStatement();
+            ResultSet rs= stmt.executeQuery(query);
+            while(rs.next()){
+                int id= rs.getInt("id");
+                String name= rs.getString("name");
+                String job_title=rs.getString("job_title");
+                double salary= rs.getDouble("salary");
+                System.out.println("**********************");
+                System.out.println("ID:" +id);
+                System.out.println("Name:"+name);
+                System.out.println("Job Title:"+job_title);
+                System.out.println("Salary:"+salary);
+            }
+            rs.close();
+            stmt.close();
+            con.close();
+            System.out.println();
+            System.out.println("Connection closed successfully");
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+}
---
+++ src/Insertion.java
@@ -0,0 +1,55 @@
+import java.sql.*;
+
+public class Insertion {
+    public static void main(String[] args) throws ClassNotFoundException {
+        String url = "jdbc:mysql://localhost:3306/mydatabase";
+        String username = "root";
+        String password = "root";
+        //String query = "INSERT INTO employees(id,name,job_title,salary) VALUES(4,'Raju','Full Stack Developer',87000)";
+        String query ="DELETE from employees where id=4;";
+        String selectQuery = "SELECT * From employees";
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("Drivers loaded successfully!!");
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            Connection con = DriverManager.getConnection(url, username, password);
+            System.out.println("Connection established successfully");
+            Statement stmt = con.createStatement();
+            int rowsaffected = stmt.executeUpdate(query);
+
+//            if (rowsaffected > 0) {
+//                System.out.println("Insert Successfull" + rowsaffected + "row(s) affected");
+//            } else {
+//                System.out.println("Insertion failed!");
+//            }
+            if (rowsaffected > 0) {
+                System.out.println("Deletion Successfull" + rowsaffected + "row(s) affected");
+            } else {
+                System.out.println("Deletion failed!");
+            }
+            ResultSet rs = stmt.executeQuery(selectQuery);
+            while (rs.next()) {
+
+                System.out.println(
+                        rs.getInt("id") + " "
+                                + rs.getString("name") + " "
+                                + rs.getString("job_title") + " "
+                                + rs.getDouble("salary")
+                );
+            }
+            rs.close();
+            stmt.close();
+            con.close();
+            System.out.println();
+            System.out.println("Connection closed successfully");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+
+    }
+}
